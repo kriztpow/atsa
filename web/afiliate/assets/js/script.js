05250d467d0c4fec8b9cc3dcb6cd6531ed93e7de
coco <cocodelacueva@gmail.com>
2018-04-11 10:48:24 -0300

    afiliate: script de validacion de formularios, falta formulario 2

--- web/afiliate/assets/js/script.js
+++ web/afiliate/assets/js/script.js
@@ -10,6 +10,84 @@
 
 var baseUrl = 'http://' + window.location.host + '/afiliate';
 var ajaxFileUrl = baseUrl + '/inc/ajax.php';
+var specialcharacters = '@#$^&%*()+=[]\'\"\/{}|:;¡!¿?<>,.';
+var numeros = '0123456789';
+var letras = 'abcdefghijklmnñopqrstuvwxyz';
+
+//busca los caracteres indicados en un string y devuelve true si existen
+function areThereAny ( cadena, characters ) {
+    for (var i = 0; i < characters.length; i++) {
+       if ( cadena.indexOf(characters[i]) != -1 ) {
+            return true;    
+       }
+   }
+   return false;
+}
+
+//quita numeros de un string
+function cleanedOthers(cadena, caracteres){ 
+
+   //eliminamos uno por uno
+   for (var i = 0; i < caracteres.length; i++) {
+       cadena= cadena.replace(new RegExp(caracteres[i], 'gi'), '');
+   }   
+
+   return cadena;
+}
+
+//quita caracteres extraños del string, los caracteres se pasan como una variable
+function cleanedSpecialCharacters(cadena, specialcharacters){ 
+
+   //eliminamos uno por uno
+   for (var i = 0; i < specialcharacters.length; i++) {
+       cadena= cadena.replace(new RegExp("\\" + specialcharacters[i], 'gi'), '');
+   }   
+
+   return cadena;
+}
+
+//lo pasa a minúsculas
+function toLowerCase(cadena) {
+    cadena = cadena.toLowerCase();
+    return cadena;
+}
+
+//remplasa dashes "-" del string por espacios
+function replaceDashes( cadena ) {
+   cadena = cadena.replace(/-/gi," ");
+   return cadena;
+}
+
+
+//borra espacios del string
+function removeDashesSpaces( cadena ) {
+   cadena = cadena.replace(/-/gi,"");
+   cadena = cadena.replace(/ /gi,"");
+   return cadena;
+}
+
+// Quitamos espacios y los sustituimos por - porque nos gusta mas asi
+function replaceSpaces( cadena ) {
+   cadena = cadena.replace(/ /gi,"-");
+   return cadena;
+}
+
+//quita acentos y ñ y lo pasa a minúsculas
+function cleanAcentos( cadena ) {
+
+   // Lo queremos devolver limpio en minusculas
+   cadena = cadena.toLowerCase();
+
+   // Quitamos acentos y "ñ". Fijate en que va sin comillas el primer parametro
+   cadena = cadena.replace(/á/gi,"a");
+   cadena = cadena.replace(/é/gi,"e");
+   cadena = cadena.replace(/í/gi,"i");
+   cadena = cadena.replace(/ó/gi,"o");
+   cadena = cadena.replace(/ú/gi,"u");
+   cadena = cadena.replace(/ñ/gi,"n");
+   return cadena;
+}
+
 
 /*
  * FORMULARIO
@@ -32,7 +110,6 @@ $(document).ready(function() {
         $(input).focus();
     }
 
-
     //clic en label, focus en input
     $(document).on('click', 'label', function(){
         focusInput( this );
@@ -44,6 +121,141 @@ $(document).ready(function() {
         $(this).addClass('input-on');
     });
 
+
+
+    /*
+     * VALIDACIONES FORMULARIO
+    */
+    
+    //KEY UP INPT (al ir escribiendo):
+    //type text, si encuentra numeros o caracteres raros devuelve la cruz de error
+    $(document).on('keyup', 'input[type=text]', function() {
+        var valor = $(this).val();
+        var contenedor = $(this).closest('.form-group');
+        var icon = $(contenedor).find('.icon-input');
+        //var msj = $(contenedor).find('.msj-error-input');
+
+        //si hay números devuelve error
+        if ( areThereAny(valor, numeros+specialcharacters) || valor=='' ) {
+            $(icon).addClass('icon-input-error');    
+        } else {
+            $(icon).removeClass('icon-input-error');    
+        }
+    });
+
+    //si encuentra letras o caracteres raros te lo indica
+    $(document).on('keyup', 'input[type=number]', function() {
+        var valor = $(this).val();
+        var contenedor = $(this).closest('.form-group');
+        var icon = $(contenedor).find('.icon-input');
+
+        //si hay letras o caracteres raro devuelve error
+        if ( areThereAny(valor, letras+specialcharacters) || valor=='' ) {
+            $(icon).addClass('icon-input-error');    
+        } else {
+            $(icon).removeClass('icon-input-error');    
+        }
+    });
+
+    //si la fecha es mayor a la de hoy, es inválida te lo indica
+    $(document).on('keyup', 'input[type=date]', function() {
+        var valor = $(this).val();
+        var contenedor = $(this).closest('.form-group');
+        var icon = $(contenedor).find('.icon-input');
+        var msj = $(contenedor).find('.msj-error-input');
+        var date = new Date(valor);
+        var today = new Date();
+
+        //si es la fecha de hoy, o la fecha de ingreso es mayor a 60 años o está vacía es nula
+        if ( today-date < 0 || today-date > 2209034488072 || valor == '' || valor == '0000-00-00' ) {
+            $(icon).addClass('icon-input-error');
+            $(msj).fadeIn();
+        } else {
+            $(icon).removeClass('icon-input-error');    
+            $(icon).addClass('icon-input-sucess');    
+            $(msj).fadeOut();  
+        }
+    });
+
+    //FOCUS OUT INPUT
+    //input text
+    $(document).on('focusout', 'input[type=text]', function() {
+        var valor = $(this).val();
+        var contenedor = $(this).closest('.form-group');
+        var icon = $(contenedor).find('.icon-input');
+        //var msj = $(contenedor).find('.msj-error-input');
+
+        valor = cleanedSpecialCharacters(valor,specialcharacters);
+        
+        valor = cleanedOthers(valor,numeros);
+        valor = replaceDashes(valor);
+        
+        $(this).val(valor);
+
+        //si hay números devuelve error
+        if ( areThereAny(valor, numeros+specialcharacters) || valor == '' || valor.length < 3) {
+            $(icon).addClass('icon-input-error');
+        } else {
+            $(icon).removeClass('icon-input-error');    
+        }
+    });
+
+    //input type numbers
+    $(document).on('focusout', 'input[type=number]', function() {
+        var valor = $(this).val();
+        var contenedor = $(this).closest('.form-group');
+        var icon = $(contenedor).find('.icon-input');
+        var msj = $(contenedor).find('.msj-error-input');
+
+        valor = cleanedOthers(valor,letras);
+       
+        valor = cleanedSpecialCharacters(valor,specialcharacters)
+        
+        $(this).val(valor);
+
+        //ver si pasa las pruebas de longitud, por defecto no las pasa
+        var longitud = true;
+
+        if ( this.name == 'dni' && valor.length == 8 ) {
+            longitud = false;
+        }
+
+        if ( ( this.name == 'cuil' || this.name == 'cuit') && valor.length == 11 ) {
+            longitud = false;
+        }
+        
+
+
+        //si hay números devuelve error
+        if ( longitud || areThereAny(valor, letras+specialcharacters) ) {
+            $(icon).addClass('icon-input-error');
+            $(msj).fadeIn();
+        } else {
+            $(icon).removeClass('icon-input-error');    
+            $(icon).addClass('icon-input-sucess');    
+            $(msj).fadeOut();
+        }
+    });
+
+    //fecha focus out
+    $(document).on('focusout', 'input[type=date]', function() {
+        var valor = $(this).val();
+        var contenedor = $(this).closest('.form-group');
+        var icon = $(contenedor).find('.icon-input');
+        var msj = $(contenedor).find('.msj-error-input');
+        var date = new Date(valor);
+        var today = new Date();
+
+        //si es la fecha de hoy, o la fecha de ingreso es mayor a 60 años o está vacía es nula
+        if ( today-date < 0 || today-date > 2209034488072 || valor == '' || valor == '0000-00-00' ) {
+            $(icon).addClass('icon-input-error');
+            $(msj).fadeIn();
+        } else {
+            $(icon).removeClass('icon-input-error');    
+            $(icon).addClass('icon-input-sucess');    
+            $(msj).fadeOut();  
+        }
+    });
 
 
     /*
