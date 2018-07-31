662363e60670bc06c6becef3da8f5013dfc11a2e
coco <coco@lacueva.tv>
2018-07-31 16:12:04 -0300

    agregue un template:_ vivo, que tiene un iframe para transmitir online por youtube

---
+++ web/inc/templates/vivo.php
@@ -0,0 +1,19 @@
+<article id="vivo" class="wrapper-page less-padding">
+
+	<h1 class="main-title-beneficios">Vivo</h1>
+
+	<div style="width: 100%;max-width: 960px;margin: 50px auto;">
+		<iframe width="100%" height="550px" src="https://www.youtube.com/embed/K6_1WlzzAXs?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+	</div>
+	<script>
+		if ( innerWidth < 960 ) {
+    		iframe = document.getElementsByTagName('iframe')[0];
+    		iframe.height = '480px';
+		}
+
+		if ( innerWidth < 550 ) {
+    		iframe = document.getElementsByTagName('iframe')[0];
+    		iframe.height = '315px';
+		}
+	</script>
+</article>
