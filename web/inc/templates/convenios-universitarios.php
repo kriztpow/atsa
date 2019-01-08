ccf9bcff992d29e5df9900c348c84053888f3ced
coco <coco@lacueva.tv>
2019-01-08 08:29:39 -0300

    modificado el front de convenios para mostrarlo tipo cursos

--- web/inc/templates/convenios-universitarios.php
+++ web/inc/templates/convenios-universitarios.php
@@ -10,61 +10,56 @@
 	    	Contamos con alianzas exclusivas con dos Universidades y una Fundación, a través de las cuales se brindan Licenciaturas, Cursos y Tecnicaturas.
 	    </p>
 
+		<div class="wrapper-cursos-no-formales">
+			<ul class="row loop-cursos-formacion-tecnica">
+			<?php
+				$connection = connectDB();
+				$tabla = 'cursos';
+				$query  = "SELECT * FROM " .$tabla. " WHERE curso_tipo='universitarios' ORDER by curso_orden ";
+					
+				$result = mysqli_query($connection, $query);
+				
+				if ( $result->num_rows == 0 ) {
+					echo 'No hay ningún convenio cargado';
+				} else {
+					while ($row = $result->fetch_array()) { ?>
+					
+					<li class="col-lg-3 col-md-4 col-sm-6">
+						<a href="/convenio/<?php echo $row['curso_slug'] ?>">
+						<article class="curso-formacion-tecnica convenio-universitario">
+							<figure>
+							<?php if ($row['curso_imagen'] != '') : ?>
+								
+								<img src="uploads/images/<?php echo $row['curso_imagen']; ?>" alt="Cursos ATSA">
+
+							<?php endif; ?>
+
+								<div class="shutter">
+									<img src="<?php echo urlBase(); ?>/assets/images/open-icon.png">
+								</div>
+
+							</figure>
+
+							<h1>
+								<?php echo $row['curso_titulo']; ?>
+							</h1>
+
+						</article><!-- //curso -->
+						</a>
+					</li><!-- //.col-md-3 .col-sm-6 -->
+
+				<?php }//while 
+				}//else
+				mysqli_close($connection);
+			?>
+			</ul>
+		</div>
+
 	    <h5 class="convenios-alert">
 	    	<strong>Mas información</strong><br>
 	    	Secretaría de Cultura al 4959-7100 (int 7117/55).
 	    </h5>
-	    <div id="universidades">
-	    <?php
-	    //busca y muestra los convenios organizados en tabs verticales de jquery ui
-	    	$connection = connectDB();
-
-			$query  = "SELECT * FROM cursos WHERE curso_tipo='universitarios' ORDER by curso_orden asc ";	
-			$result = mysqli_query($connection, $query);
-			
-			if ( $result->num_rows == 0 ) {
-				return;
-			} else {
-				while ( $row = $result->fetch_array() ) {
-					$rows[] = $row;
-				}//while
-
-
-				//agrupar datos
-				$titulos = array();
-				$contenidos = array();
-				$idHref = array();
-				for ($i=0; $i < count($rows); $i++) { 
-					array_push($titulos, $rows[$i]['curso_titulo']);
-					array_push($contenidos, $rows[$i]['curso_objespecifico']);
-					$newID = 'idConvenio-'.$rows[$i]['curso_ID'];
-					array_push($idHref, $newID);
-				}//for
-
-				//imprimir titulos
-			?> <ul> <?php
-				for ($i=0; $i < count($rows); $i++) { ?>
-					<li>
-						<a href="#<?php echo $idHref[$i]; ?>" class="urltochange">
-							<?php echo $titulos[$i]; ?>
-						</a>
-					</li>
-			<?php }//for
-			?> </ul> <?php
-				//imprimir contenidos
-				for ($i=0; $i < count($rows); $i++) { ?>
-					<div id="<?php echo $idHref[$i]; ?>" class="tabs-content">
-						<?php echo $contenidos[$i]; ?>
-					</div>
-			<?php }//for
-
-
-
-			}//else
-
-	    	mysqli_close($connection);
-	    ?>
-	    </div>
+	    
 	</div>
 </article>
 <script src="js/jquery-ui.min.js"></script>
